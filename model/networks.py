4836a04c7a99adb3a04e28e0e65b233b61045164
Chuxiangbo Wang <157438849+ChuxiangboWang@users.noreply.github.com>
2025-12-04 13:09:42 -0500

    networks.py

--- model/networks.py
+++ model/networks.py
@@ -82,6 +82,7 @@ def init_weights(net, init_type='kaiming', scale=1, std=0.02):
 # Generator
 def define_G(opt):
     model_opt = opt['model']
+    vgg_opt   = model_opt.get('vgg', None)
     if model_opt['which_model_G'] == 'sr3':
         from .sr3_modules import diffusion, unet
     if ('norm_groups' not in model_opt['unet']) or model_opt['unet']['norm_groups'] is None:
